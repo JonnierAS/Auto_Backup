728432725103130480082e5d3852f3f4ad3c558b
Jonnier <jonniersalaverria@gmail.com>
2023-11-30 11:28:58 -0500

    auto backup funcionando

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-// const{ CronJob } = require('cron');
-
-
-// console.log('Before job instantiation');
-// const job = new CronJob('0 */1 14-22 * * *', function () {
-// 	const d = new Date();
-//     d.setHours(d.getHours() - 5)
-// 	console.log('Every 30 minutes between 9-17:', d);
-// });
-// console.log('After job instantiation');
-// job.start();
 const { CronJob } = require('cron');
 const { exec } = require('child_process');
-
+require('dotenv').config();
 console.log('Before job instantiation');
 
-const job = new CronJob('0 */1 14-22 * * *', function () {
-    const d = new Date();
-    d.setHours(d.getHours() - 5);
-    console.log('Every 30 minutes between 9-17:', d);
-    const fecha = d.toString();
-    console.log(typeof(fecha));
-    exec(`cmd /c start /min C:\\Program" "Files\\PostgreSQL\\15\\bin\\pg_dump --host localhost --port 5432 --username "postgres" --format custom --file "C:\\Users\\USUARIO\\Desktop\\Respaldos_BD\\pruebaGeo_${fecha}.backup" "pruebaGeo"`, (error, stdout, stderr) => {
+const { DB, HOST, PORT, USER } = process.env;
+
+const job = new CronJob('9 8,11 * * *', function () {
+    const currentDate = new Date();
+    currentDate.setHours(currentDate.getHours() - 5);
+
+    const formattedDate = currentDate.toLocaleString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZone: 'UTC'
+    }).replace(/[\/\,\:\s]/g, "_");
+
+    console.log('Every 270 minutes between 9-17:', formattedDate);
+
+    const command = `cmd /c start /min C:\\Program" "Files\\PostgreSQL\\15\\bin\\pg_dump --host ${HOST} --port ${PORT} --username ${USER} --format custom --file "C:\\Users\\USUARIO\\Desktop\\Respaldos_BD\\pruebaGeo_${formattedDate}.sql" ${DB}`;
+
+    exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing backup script: ${error}`);
+            console.error(`Error details: ${stderr}`);
             return;
         }
         console.log(`Backup script output: ${stdout}`);
-        console.error(`Backup script errors: ${stderr}`);
+        console.log(`Backup script errors: ${stderr}`);
     });
 });
 
